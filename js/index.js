d1a340b7875c43e004b9f64c21f1e63e929d2595
Mihael Herenda <141841668+MihaelHerenda@users.noreply.github.com>
2023-08-30 14:05:54 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,91 @@
+let green = new Audio("/sounds/green.mp3");
+let yellow = new Audio("/sounds/yellow.mp3");
+let red = new Audio("/sounds/red.mp3");
+let blue = new Audio("/sounds/blue.mp3");
+let wrong = new Audio("/sounds/wrong.mp3");
+let gameStarted = false;
+let level = 0;
+let orderOfSquaresArr = [];
+let pressCounter = 0;
+const squareNumberDict = {
+  1: "green",
+  2: "yellow",
+  3: "red",
+  4: "blue",
+};
+
+const squareSoundDict = {
+  1: green,
+  2: yellow,
+  3: red,
+  4: blue,
+};
+
+function resetAll() {
+  $("h1").html("You lost at level " + level + ".");
+}
+
+function wrongPress() {
+  let wrong = new Audio("/sounds/wrong.mp3");
+  wrong.play();
+  $("body").css("background-color", "red");
+  setTimeout(function () {
+    $("body").css("background-color", "#183D3D");
+  }, 200);
+}
+
+function generateSquare() {
+  level++;
+  var squareNr = Math.ceil(Math.random() * 4);
+  squareSoundDict[squareNr].play();
+  orderOfSquaresArr.push(squareNr);
+  let nextSquare = squareNumberDict[squareNr];
+  $("." + nextSquare)
+    .animate({ opacity: 0.2 })
+    .animate({ opacity: 1 });
+  $("h1").html("Level " + level);
+  return squareNr;
+}
+
+$("body").keypress(function (e) {
+  if (gameStarted) {
+    wrongPress();
+    return;
+  } else {
+    gameStarted = true;
+    generateSquare();
+  }
+});
+
+$(".start-button").click(function (e) {
+  if (gameStarted) {
+    wrongPress();
+    return;
+  } else {
+    gameStarted = true;
+    $("h1").html("Level 1");
+    setTimeout(generateSquare, 500);
+  }
+});
+
+$(".button-style").click(function (e) {
+  e.preventDefault();
+  if (gameStarted == false) {
+    wrongPress();
+    pressCounter = 0;
+    return;
+  } else if (e.target.id == orderOfSquaresArr[pressCounter]) {
+    squareSoundDict[e.target.id].play();
+    pressCounter++;
+    if (pressCounter == orderOfSquaresArr.length) {
+      pressCounter = 0;
+      setTimeout(generateSquare, 750);
+    }
+  } else {
+    wrongPress();
+    resetAll();
+    pressCounter = 0;
+    level = 0;
+    gameStarted = false;
+  }
+});
